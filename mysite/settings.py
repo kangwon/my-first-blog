82acc972356867ff68e6042c9af28647f591a227
kangwon <kang_won@naver.com>
2017-03-30 17:04:15 +0900

    allow host all

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y24up=&rz(_r0btp*wl5x*ksnsv538b+%gj8pabo$^pgf*_6%2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
